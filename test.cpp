2b1fe05502ac93f8e354f83d929e0f9f6f6e18d4
Om Morendha <omkmorendha@gmail.com>
2024-07-21 23:45:45 +0530

    Add set matrix 0

--- test.cpp
+++ test.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
+    int i = 0;
     std::cout << "Hello World" << std::endl;
+
+    i++;
+    std::cout<< "Test" << std::endl;
+    i++;
+
+    std::cout<< "Done" << std::endl;
 
     return 0;
 }
