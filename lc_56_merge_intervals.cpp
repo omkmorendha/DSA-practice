b49cf8d98a4a1fe03b92e6edaec2251ec2d51a80
Om Morendha <omkmorendha@gmail.com>
2024-07-30 22:22:07 +0530

    Optimise merge intervals

--- lc_56_merge_intervals.cpp
+++ lc_56_merge_intervals.cpp
@@ -4,27 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    static bool comp(vector<int> a, vector<int> b){
-        return a[0] < b[0];
-    }
-
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> new_arr(intervals); 
-        sort(new_arr.begin(), new_arr.end(), comp);
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
 
-        int index = 0;
+        int start = intervals[0][0], end = intervals[0][1];
 
         for(int i = 1; i < intervals.size(); i++){
-            if(new_arr[i][0] > new_arr[index][1]){
-                index++;
-                new_arr[index] = new_arr[i];
+            if(intervals[i][0] > end){
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
             else{
-                new_arr[index][1] = max(new_arr[index][1], new_arr[i][1]);
+                end = max(end, intervals[i][1]);
             }
         }
 
-        new_arr.resize(index + 1);
-        return new_arr;
+        ans.push_back({start, end});
+        return ans;    
     }
 };
